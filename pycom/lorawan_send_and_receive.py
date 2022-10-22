054ced6ff7048e230295de031eb66d3f7a392b1a
Alex Fernandez <javier.fddz@gmail.com>
2022-10-22 18:47:38 +0200

    Def app eui, key, add printouts

--- pycom/lorawan_send_and_receive.py
+++ pycom/lorawan_send_and_receive.py
@@ -4,6 +4,10 @@ import time
 import pycom
 import binascii
 
+# pycom params
+my_app_eui = '0000000000000000'
+my_app_key = '72457475581217131977635496807027'
+
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 #
 mac = lora.mac()
@@ -11,27 +15,31 @@ print ('devEUI: ',  binascii.hexlify(mac))
 
 # create an OTAA authentication parameters
 app_eui = binascii.unhexlify(
-     '70B3D57ED0033AE3'.replace(' ',''))
-#     '0000000000000000'.replace(' ',''))
+    my_app_eui.replace(' ',''))
 
 app_key = binascii.unhexlify(
-     #'B0923EE49E056F29C1482EE5846FADF4'.replace(' ',''))  # TTN
-     #'11223344556677881122334455667788'.replace(' ',''))   # Acklio
-     '11 00 22 00 33 00 44 00 55 00 66 00 77 00 88 00'.replace(' ',''))   # chirpstack
+    my_app_key.replace(' ',''))
+    #'B0923EE49E056F29C1482EE5846FADF4'.replace(' ',''))  # TTN
+    #'11223344556677881122334455667788'.replace(' ',''))  # Acklio
+    #'11 00 22 00 33 00 44 00 55 00 66 00 77 00 88 00'.replace(' ',''))  # chirpstack
 
 pycom.heartbeat(False)
 pycom.rgbled(0x101010) # white
+print("> Joining LoRa...")
 
 # join a network using OTAA (Over the Air Activation)
-lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key),  timeout=0)
+lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), 
+    timeout=0)
 
 # wait until the module has joined the network
 while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
 
+print("> Joined LoRa.")
 pycom.rgbled(0x000000) # black
 
+# Passer les params a la couche protocolaire LoRa
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 s.setsockopt(socket.SOL_LORA,  socket.SO_CONFIRMED,  False)
@@ -41,22 +49,26 @@ while True:
     s.setblocking(True)
     s.settimeout(10)
 
+    print("> Sending msg...")
     try:
         s.send('Hello LoRa')
+        print("  > Msg sent!")
     except:
-        print ('timeout in sending')
+        print ('  timeout in sending')
 
     pycom.rgbled(0x000010) # blue
+    print("> Awaiting msg...")
 
     try:
         data = s.recv(64)
 
         print(data)
         pycom.rgbled(0x001000) # green
+        print("  > Msg received!")
     except:
-        print ('timeout in receive')
+        print ('  timeout in receive')
         pycom.rgbled(0x000000) # black
 
-
+    print(" z Z z z")
     s.setblocking(False)
     time.sleep (29)
